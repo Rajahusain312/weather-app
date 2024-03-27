a7e0549b49453556b109e069f78504af8e881fc8
Rajahusain312 <rajahusain312@gmail.com>
2024-03-27 21:04:22 +0530

    docs: script func completed

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
 
-    console.log(data);
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
     
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
@@ -28,6 +31,13 @@ async function checkWeather(city) {
     } else if (data.weather[0].main == "Mist") {
         weatherIcon.src = "/assets/images/mist.png";
     }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+    
+    }
+
+    
 }
 
 searchBtn.addEventListener("click", () => {
